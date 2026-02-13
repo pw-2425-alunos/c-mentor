efbd70e332fd503a6b3c01ce5f1e82a7998ed512
Lucio Studer Ferreira <42048382+luciostuder@users.noreply.github.com>
2026-02-13 12:26:38 +0000

    Add WhiteNoiseMiddleware to MIDDLEWARE settings

--- projeto/settings.py
+++ projeto/settings.py
@@ -86,6 +86,7 @@ LOGGING = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
